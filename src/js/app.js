d8da2f5a3c16eed27e30819b74589b58a94fd928
Vasile-Bogdan Cărbune <BogdanCar@yonder.local>
2019-03-22 23:38:48 +0200

    Added few tweaks to inputs & buttons default outline, added login and register to router, added container styles to show pages properly, deleted home & menu test pages, custom text selection highlight added

--- src/js/app.js
+++ src/js/app.js
@@ -4,13 +4,15 @@ import Page from './utils/page.js';
 
 class App {
     constructor() {
+        this.state={
+            
+        }
         this.router = new Router(
             {
                 login: new Page('login.html'),
+                register: new Page('register.html'),
                 newsfeed: new Page('news-feed.html'),
                 topics: new Page('topics.html'),
-                about: new Layout(new Page('menu.html'), new Page('about.html')),
-                home: new Layout(new Page('menu.html'), new Page('home.html')),
                 default: new Page('general-layout.html'),
             },
             document.getElementById('router')
