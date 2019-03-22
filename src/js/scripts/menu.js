e6d701b9774fab216b7c27aeabd90f9dcbcc8846
Turcu Stefanel <you@example.com>
2019-03-22 21:50:50 +0200

    Fixed class Menu

--- src/js/scripts/menu.js
+++ src/js/scripts/menu.js
@@ -1,6 +1,6 @@
 export default class Menu {
     showMenu() {
-        var elements = document.getElementById("mobile-menu");
+        var elements = document.getElementsByClassName("mobile-menu");
 
         elements[0].classList.add("animated-in");
         elements[1].classList.add("animate-overlay-in");
@@ -11,7 +11,7 @@ export default class Menu {
     }
 
     closeMenu() {
-        var elements = document.getElementById("mobile-menu");
+        var elements = document.getElementsByClassName("mobile-menu");
 
         for(var i = 0; i < elements.length; i++) {
             elements[i].style.display = 'none';
