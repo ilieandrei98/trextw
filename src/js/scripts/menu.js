72653753edaf90c763ce76e9fa1dc768dfac871a
Turcu Stefanel <you@example.com>
2019-03-22 21:47:28 +0200

    Added search bar on mobile

--- src/js/scripts/menu.js
+++ src/js/scripts/menu.js
@@ -1,6 +1,6 @@
-export default class menu {    
-    static showMenu() {
-        var elements = document.getElementsByClassName("mobile-menu");
+export default class Menu {
+    showMenu() {
+        var elements = document.getElementById("mobile-menu");
 
         elements[0].classList.add("animated-in");
         elements[1].classList.add("animate-overlay-in");
@@ -10,8 +10,8 @@ export default class menu {
         }
     }
 
-    static closeMenu() {
-        var elements = document.getElementsByClassName("mobile-menu");
+    closeMenu() {
+        var elements = document.getElementById("mobile-menu");
 
         for(var i = 0; i < elements.length; i++) {
             elements[i].style.display = 'none';
