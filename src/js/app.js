ea0b65321f0ee546514d87d7577ed176f7121a4a
Ilie Andrei <ilandrei98@gmail.com>
2019-03-22 22:32:25 +0200

    i will commit suicide

--- src/js/app.js
+++ src/js/app.js
@@ -6,9 +6,11 @@ class App {
     constructor() {
         this.router = new Router(
             {
+                newsfeed: new Page('news-feed.html'),
+                topics: new Page('topics.html'),
                 about: new Layout(new Page('menu.html'), new Page('about.html')),
                 home: new Layout(new Page('menu.html'), new Page('home.html')),
-                default: new Page('general-layout.html'),                
+                default: new Page('general-layout.html'),
             },
             document.getElementById('router')
         );
